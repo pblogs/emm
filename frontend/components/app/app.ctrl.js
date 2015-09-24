1f66ef8c40c8e0045d890a193341d59c98cd2c79
Anthony Panepinto <anthony.panepinto101@gmail.com>
2015-09-24 16:51:15 +0300

    sign-in

--- frontend/components/app/app.ctrl.js
+++ frontend/components/app/app.ctrl.js
@@ -3,12 +3,29 @@
 angular.module('app')
   .controller('AppCtrl', function($scope, $modal) {
     $scope.signUpModal = signUpModal;
+    $scope.signInModal = signInModal;
 
     function signUpModal() {
       $modal.open({
         templateUrl: 'components/auth/sign-up/modal.html',
         controller: 'SignUpModalCtrl',
         windowClass: 'e-modal'
-      })
+      }).result.catch(function(reason) {
+        if (reason === 'signIn') {
+          signInModal();
+        }
+      });
+    }
+
+    function signInModal() {
+      $modal.open({
+        templateUrl: 'components/auth/sign-in/modal.html',
+        controller: 'SignInModalCtrl',
+        windowClass: 'e-modal'
+      }).result.catch(function(reason) {
+        if (reason === 'signUp') {
+          signUpModal();
+        }
+      });
     }
   });
