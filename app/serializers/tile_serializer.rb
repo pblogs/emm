69474af16863f7172a46614a3ed9fb5e0ab644e5
Anthony Panepinto <anthony.panepinto101@gmail.com>
2015-10-02 12:30:59 +0300

    feedbacks

--- app/serializers/tile_serializer.rb
+++ app/serializers/tile_serializer.rb
@@ -2,4 +2,8 @@ class TileSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :weight, :size, :content_type
 
   has_one :content
+
+  def content_type
+    object.content_type.downcase
+  end
 end
