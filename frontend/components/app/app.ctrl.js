f4fa413ce05a31942afc8e44afbaf5ce58ae14f7
Anthony Panepinto <anthony.panepinto101@gmail.com>
2015-10-16 18:10:01 +0300

    add visible-user dropdown

--- frontend/components/app/app.ctrl.js
+++ frontend/components/app/app.ctrl.js
@@ -1,16 +1,31 @@
 'use strict';
 
 angular.module('app')
-  .controller('AppCtrl', function ($scope, CurrentUser, $auth, $state, AuthModal, MediaTypeSelectModal, currentUser, Background) {
+  .controller('AppCtrl', function ($scope, CurrentUser, $auth, $state, AuthModal, MediaTypeSelectModal, currentUser, Background, Restangular, $rootScope) {
     $scope.AuthModal = AuthModal;
     $scope.MediaTypeSelectModal = MediaTypeSelectModal;
     $scope.Background = Background;
     $scope.logout = logout;
     $scope.currentUser = currentUser;
 
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+      setVisibleUser();
+    });
+
+    function setVisibleUser() {
+      if ($state.params.user_id) {
+        Restangular.one('users', $state.params.user_id).get().then(function(user) {
+          $scope.visibleUser = user;
+        })
+      } else {
+        $scope.visibleUser = $scope.currentUser;
+      }
+    }
+
     // When user logs in/out we reload CurrentUser service and all bindings will be updated
     $scope.$watch($auth.isAuthenticated, function(newVal, oldVal) {
       if (newVal != oldVal) CurrentUser.reload();
+      setVisibleUser();
     });
 
     function logout() {
