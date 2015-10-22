293be30ea6e7f360779951e542115c16e7830dee
Anthony Panepinto <anthony.panepinto101@gmail.com>
2015-10-22 18:07:46 +0300

    relations fixes

--- app/controllers/users_search_controller.rb
+++ app/controllers/users_search_controller.rb
@@ -1,6 +1,7 @@
 class UsersSearchController < ApplicationController
   def index
-    result = User.search_by_filter(params['filter'])
-    render_resources(result.page(params[:page]).per(params[:per]))
+    result = User.search_by_filter(params['filter']).where.not(role: User.roles['admin'])
+    statuses = current_user.relationships_statuses
+    render_resources(result.page(params[:page]).per(params[:per]), statuses: statuses)
   end
 end
