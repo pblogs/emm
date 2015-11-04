495509ea8d6d65f914deb27582da0f46f27c7c5e
Anthony Panepinto <anthony.panepinto101@gmail.com>
2015-11-04 15:02:15 +0200

    add tiles to tributes

--- app/serializers/tribute_serializer.rb
+++ app/serializers/tribute_serializer.rb
@@ -1,5 +1,9 @@
 class TributeSerializer < LikableContentSerializer
-  attributes :id, :user_id, :author_id, :description, :created_at
+  attributes :id, :user_id, :author_id, :description, :created_at, :tile
 
   has_one :author
+
+  def tile
+    TileSerializer.new(object.tile, root: false, skip_content: true).as_json if options[:with_tile]
+  end
 end
--- spec/factories/tributes.rb
+++ spec/factories/tributes.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :tribute do
     association :author, factory: :user
     association :user
-    title { Faker::Lorem.sentence(1, false, 5) }
     description { Faker::Lorem.paragraph(1, false, 5) }
   end
 end
