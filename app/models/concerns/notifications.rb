a505f577be86e33c4b726729f5187adeeb67d913
Anthony Panepinto <anthony.panepinto101@gmail.com>
2015-11-16 14:58:29 +0200

    fix notification for like or comment on own record

--- app/models/concerns/notifications.rb
+++ app/models/concerns/notifications.rb
@@ -9,8 +9,11 @@ module Notifications
   private
 
   def create_notifications
+    event = self.class.name.downcase
     (@notification_users_ids || []).each do |user_id|
+      next if (event == 'like' && self.user_id == user_id) || (event == 'comment' && self.author_id == user_id)
       self.notifications.create(event: self.class.name.downcase, user_id: user_id)
     end
+    true
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -4,16 +4,32 @@ RSpec.describe Notification, type: :model do
   let(:user) { create(:user, :confirmed) }
   let(:another_user) { create(:user, :confirmed) }
 
-  it 'should create notification for like' do
-    expect{
-      create(:like, target: user.default_album, user: another_user)
-    }.to change(Notification.where(event: Notification.events['like']), :count).by 1
+  describe 'like' do
+    it 'should create notification for like' do
+      expect{
+        create(:like, target: user.default_album, user: another_user)
+      }.to change(Notification.where(event: Notification.events['like']), :count).by 1
+    end
+
+    it 'should not create notification for like to own record' do
+      expect{
+        create(:like, target: user.default_album, user: user)
+      }.to change(Notification.where(event: Notification.events['like']), :count).by 0
+    end
   end
 
-  it 'should create notification for comment' do
-    expect{
-      create(:comment, commentable: user.default_album, author: another_user)
-    }.to change(user.notifications.where(event: Notification.events['comment']), :count).by 1
+  describe 'comment' do
+    it 'should create notification for comment' do
+      expect{
+        create(:comment, commentable: user.default_album, author: another_user)
+      }.to change(user.notifications.where(event: Notification.events['comment']), :count).by 1
+    end
+
+    it 'should not create notification for comment to own record' do
+      expect{
+        create(:comment, commentable: user.default_album, author: user)
+      }.to change(user.notifications.where(event: Notification.events['comment']), :count).by 0
+    end
   end
 
   it 'should create notification for tribute' do
